5ff61cda8994ff20336ea498103fb7387c8e4aee
otake <otake.hiroyo@gmail.com>
2016-12-13 18:37:48 +0900

    指摘事項修正_update

--- app/Controller/ProfitsController.php
+++ app/Controller/ProfitsController.php
@@ -17,23 +17,14 @@ class ProfitsController extends AppController {
 	
 	public function add() {
 		if ($this->request->is('post')) {
-			$this->Profit->create(); //insurtの準備
-			$result = $this->Profit->save($this->request->data); //insurtもupdateもsaveを使う
-			  	// こういう形のデータを保存  $this->Profit->save(['Profit'=> ['earnings' => 1]]); 
 
 			$profit = $this->request->data;
-			$earnings = $profit['Profit']['earnings'];
+			$profit['Profit']['date'] = date('Y-m-d');
+
+			$this->Profit->create(); 
+			$result = $this->Profit->save($profit); 
 
 			if($result) { 			
-				$sql = "select MAX(id) from profits"; //あとでログイン機能を付けたときに,メンバーIDで条件付けする 
-				$array = $this->Profit->query($sql);
-				$id = $array[0][0]['MAX(id)'];
-
-				$now = date('Y-m-d');
-
-				$this->Profit->create(); 
-				$this->Profit->save(['Profit' => ['id' => $id,'date' => $now]]); 
-
 				$this->Flash->success('売上金額を登録しました');
 				$this->redirect(['action' => 'index']);
 				return;
